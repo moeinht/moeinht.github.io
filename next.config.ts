89cc166000805169081456d892678e8261e403a0
Moein Hoseini <moeinhoseini@Moeins-MacBook-Air.local>
2026-06-22 15:04:38 +0330

    nextjs github pages setup

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next"
-const repoName = "moeinht.github.io"
 
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
 }
 export default nextConfig
